106d3eb2eace18764bfee85cde7349090d4542e3
Vinnyg96 <vinnyg96@homtmail.com>
2020-04-09 01:07:32 -0400

    UI Changes -Changed color pallet -Added drop shadows to controls -Added icons to controls

--- ShipContentManager/MainWindow.xaml.cs
+++ ShipContentManager/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using ShipContentManager.Models;
 using ShipContentManager.Services;
 
